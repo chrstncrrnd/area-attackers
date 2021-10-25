f1538af9958a70ead184c52f7c6f576da7358779
ChrstnCrrnd <chrstncrrnd@gmail.com>
2021-10-26 00:53:38 +0200

    added the tank and a temporary texture lol

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+mod tank;
+
 use macroquad::prelude::*;
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
+    let tank_texutre = load_texture("player_texture_default.png").await.unwrap();
+    let mut tank = tank::Tank::new(tank_texutre);
     loop {
         clear_background(WHITE);
 
         draw_text("Wow very space invaders!", 100., 100., 100., BLACK);
+
+        tank.render();
 
         next_frame().await
     }
---
+++ src/tank.rs
@@ -0,0 +1,43 @@
+use macroquad::prelude::*;
+
+
+///# Tank struct
+/// The tank is what the player will be controlling
+/// pos: position
+/// speed: the tank speed
+/// sprite: the texture that will be rendered
+pub struct Tank{
+    pub pos: Vec2,
+    pub speed: u8,
+    pub sprite: Texture2D,
+    pub size: Vec2
+}
+
+
+impl Tank{
+    /// New function to sort of standardise the way it will
+    /// be initialised
+    /// takes its sprite as an argument
+    pub fn new(sprite: Texture2D) -> Self{
+        Self{
+            pos: Vec2::new(0.,0.),
+            speed: 10,
+            sprite,
+            size: Vec2::new(100., 100.)
+        }
+    }
+
+    pub fn render(&mut self){
+        draw_texture_ex(
+            self.sprite,
+            self.pos.x,
+            self.pos.y,
+            WHITE,
+            DrawTextureParams{
+                dest_size: Option::from(self.size),
+                ..Default::default()
+            }
+        )
+    }
+
+}
