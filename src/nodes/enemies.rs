8c972cce0e367e0b92040b354c4153ed8b95db44
ChrstnCrrnd <chrstncrrnd@gmail.com>
2021-11-03 13:16:02 +0100

    centered but it is very spaghetti

--- src/nodes/enemies.rs
+++ src/nodes/enemies.rs
@@ -1,6 +1,7 @@
 use macroquad::color::WHITE;
 use macroquad::math::{Vec2, vec2};
-use macroquad::prelude::{draw_texture_ex, DrawTextureParams, Texture2D};
+use macroquad::prelude::{draw_texture_ex, DrawTextureParams, screen_height, Texture2D};
+use macroquad::window::screen_width;
 
 //amount of enemies
 const AMOUNT_OF_ENEMIES: u8 = 40;
@@ -76,21 +77,25 @@ impl Enemy{
     /// Create a new enemy with only its index and its texture
     /// this contains all of the setup code
     pub fn new(index: u8, texture: &Texture2D) -> Self{
-
-        let width_of_enemies = ENEMIES_PER_LAYER as i32 * PADDING_X;
-
-        //this is a bit spaghetti will try to clean up
+        //this is a bit spaghetti will try to clean up but hopefully well commented
+        //declare the size because we are going to need it later
+        let size = vec2(20.,35.);
+        //get the width of enemies (we subtract 1 below because it will count 1 extra that is at the edge multiply it by the space between and you get width
+        let width_of_enemies = (ENEMIES_PER_LAYER - 1) as i32 * PADDING_X;
+        //offset is the screen width take the width of the enemies and divided by two
+        //we then take the size of the sprite divided by two because the position isnt at the center
+        let offset = ((screen_width() as i32 - width_of_enemies) / 2 ) - (size.x / 2.) as i32;
         //calculates what layer this enemy is on
         let layer = (index / ENEMIES_PER_LAYER);
         //calculates its position but pushed back if its on a new layer
         //that is what layer * ENEMIES_PER_LAYER does
-        let position_x: f32 = ((index - layer * ENEMIES_PER_LAYER) as i32 * PADDING_X) as f32;
+        let position_x: f32 = (((index - layer * ENEMIES_PER_LAYER) as i32 * PADDING_X) + offset) as f32;
         //just creates the vec2 for the position | below is just spacing out the layers in the y axis
         let position = vec2(position_x, (PADDING_Y * layer as i32) as f32);
         Self{
             position,
             //the image is: 120 x 217
-            size: vec2(20., 35.),
+            size,
             index,
             texture: *texture
         }
