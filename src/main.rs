943b0d59c83d44bcbb51ab3c90c364820044f5e7
ChrstnC <chrstncrrnd@gmail.com>
2022-05-07 23:28:10 +0200

    added load macro and fixed overlap function

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,7 @@ async fn main() {
     let mut tank = tank::Tank::new(game_resources.tank, game_resources.tank_projectile);
     //new enemies
     let mut enemies = Enemies::new(game_resources.enemy, game_resources.enemy_projectile);
+
     println!("Done loading!");
     loop {
         //clear the background
@@ -40,6 +41,7 @@ async fn main() {
         //render the tank
         tank.render();
         enemies.render();
+
         // new scope to handle enemy death logic (yes i know that im only creating enemy_to_remove and
         // its the only thing that will get dropped, the new scope is just here to organise a bit more idk
         {
@@ -79,37 +81,40 @@ async fn main() {
 
 
 //function to see if an enemy is hit by a tank projectile
+#[inline]
 fn enemy_tproj_overlap(enemy: &Enemy, tproj: &TankProjectile) -> bool {
     overlaps(
         enemy.position,
         vec2(
             enemy.position.x + enemy.size.x,
-            enemy.position.y - enemy.size.y,
+            enemy.position.y + enemy.size.y,
         ),
         tproj.position,
         vec2(
             tproj.position.x + tproj.size.x,
-            tproj.position.y - tproj.size.y,
+            tproj.position.y + tproj.size.y,
         ),
     )
 }
 
 //function to see if the tank is hit by an enemy projectile
+#[inline]
 fn tank_eproj_overlap(tank: &Tank, eproj: &EnemyProjectile) -> bool {
     overlaps(
         tank.pos,
         vec2(
             tank.pos.x + tank.size.x,
-            tank.pos.y - tank.size.y
+            tank.pos.y + tank.size.y
         ),
         eproj.position,
         vec2(
             eproj.position.x + eproj.size.x,
-            eproj.position.y - eproj.size.y,
+            eproj.position.y + eproj.size.y,
         ),
     )
 }
 
+#[inline]
 fn overlaps(tl_1: Vec2, br_1: Vec2, tl_2: Vec2, br_2: Vec2) -> bool {
-    tl_1.y > br_2.y && br_1.y < tl_2.y && br_1.x > tl_2.x && tl_1.x < br_2.x
+    br_1.x > tl_2.x && br_2.x > tl_1.x && br_1.y > tl_2.y && br_2.y > tl_1.y
 }
--- src/resources.rs
+++ src/resources.rs
@@ -1,13 +1,12 @@
 //! # This is the resource file loading file
 //! yeah it loads resources from disk
-use macroquad::miniquad::FilterMode;
-use macroquad::prelude::{FileError, Font, load_texture, Texture2D};
+use macroquad::prelude::{FileError, Font, Texture2D};
 use macroquad::text::load_ttf_font;
+
 
 /// # Resources struct
 /// this contains all of the game required resources.
 /// there arent many so its fine for now
-/// TODO: Add a texture atlas for more efficient loading
 pub struct Resources {
     pub enemy: Texture2D,
     pub enemy_projectile: Texture2D,
@@ -17,42 +16,30 @@ pub struct Resources {
     pub font: Font,
 }
 
+// Macro for DRY loading
+macro_rules! load {
+        ($($var_name:ident),+) => {
+            $(
+                let var_name_str = stringify!($var_name);
+                let path = format!("assets/{}.png", var_name_str);
+                let $var_name = macroquad::prelude::load_texture(path.as_str()).await.unwrap();
+                $var_name.set_filter(macroquad::prelude::FilterMode::Nearest);
+            )+
+        };
+    }
+
 /// actual loader for the resources
 impl Resources {
     //new function, returns the resources or a file error if something goes wrong
     pub async fn load() -> Result<Resources, FileError> {
         println!("Starting resource loading");
 
-        //all of the image loadings do the same thing might be able to add a for loop idk
-        //1) set a non mut variable to load the texture from its file ./ being where src/ and Cargo.toml are
-        let enemy = load_texture("assets/enemy.png").await.unwrap();
-        //2) set the filter to make sure that its not blurred or anything
-        enemy.set_filter(FilterMode::Nearest);
-        //3) print it has been successful
-        println!("Loaded 1/6 assets");
-
-        let enemy_projectile = load_texture("assets/enemy_projectile.png").await.unwrap();
-        enemy_projectile.set_filter(FilterMode::Nearest);
-        println!("Loaded 2/6 assets");
-
-        let tank = load_texture("assets/tank.png").await.unwrap();
-        tank.set_filter(FilterMode::Nearest);
-        println!("Loaded 3/6 assets");
-
-        let tank_projectile = load_texture("assets/tank_projectile.png").await.unwrap();
-        tank_projectile.set_filter(FilterMode::Nearest);
-        println!("Loaded 4/6 assets");
-
-        let background = load_texture("assets/background.png").await.unwrap();
-        background.set_filter(FilterMode::Nearest);
-        println!("Loaded 5/6 assets");
-
-        //font is the same as the images except there is no filter
+        load!(enemy, enemy_projectile, tank, tank_projectile, background);
+        // not really worth putting font in the load macro, if there is another font, ill make a font loading macro
         let font = load_ttf_font("assets/font.ttf").await.unwrap();
-        println!("Loaded 6/6 assets");
+
         println!("Finished loading assets");
         //if everything goes well return resources with all of the previous variables
-        //returns it in the shorthand state
         Ok(Resources {
             enemy,
             enemy_projectile,
@@ -63,3 +50,4 @@ impl Resources {
         })
     }
 }
+
