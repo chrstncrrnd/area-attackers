7dda5e247a39613bf21c229186431f12a3e71a69
ChrstnC <chrstncrrnd@gmail.com>
2022-05-08 00:23:08 +0200

    nothing

--- src/resources.rs
+++ src/resources.rs
@@ -1,6 +1,6 @@
 //! # This is the resource file loading file
 //! yeah it loads resources from disk
-use macroquad::prelude::{FileError, Font, Texture2D};
+use macroquad::prelude::{FileError, Font, load_texture, Texture2D};
 use macroquad::text::load_ttf_font;
 
 
@@ -18,27 +18,26 @@ pub struct Resources {
 
 // Macro for DRY loading
 macro_rules! load {
-        ($($var_name:ident),+) => {
-            $(
-                let var_name_str = stringify!($var_name);
-                let path = format!("assets/{}.png", var_name_str);
-                let $var_name = macroquad::prelude::load_texture(path.as_str()).await.unwrap();
-                $var_name.set_filter(macroquad::prelude::FilterMode::Nearest);
-            )+
-        };
-    }
+    ($($var_name:ident),+) => {
+        $(
+            let var_name_str = stringify!($var_name);
+            let path = format!("assets/{}.png", var_name_str);
+            let $var_name = macroquad::prelude::load_texture(path.as_str()).await.unwrap();
+            $var_name.set_filter(macroquad::prelude::FilterMode::Nearest);
+        )+
+    };
+}
+
+
+
 
 /// actual loader for the resources
 impl Resources {
     //new function, returns the resources or a file error if something goes wrong
     pub async fn load() -> Result<Resources, FileError> {
-        println!("Starting resource loading");
-
         load!(enemy, enemy_projectile, tank, tank_projectile, background);
-        // not really worth putting font in the load macro, if there is another font, ill make a font loading macro
+        // not really worth putting font in the load macro.
         let font = load_ttf_font("assets/font.ttf").await.unwrap();
-
-        println!("Finished loading assets");
         //if everything goes well return resources with all of the previous variables
         Ok(Resources {
             enemy,
